d235b2c3307c72be1d4301a03e7f170eb9dce991
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-21 17:02:18 +0600

    Fix unified Service Payment table wrapper

--- src/components/ServicePaymentLeanPage.tsx
+++ src/components/ServicePaymentLeanPage.tsx
@@ -331,8 +331,8 @@ export const ServicePaymentLeanPage: React.FC<{
               )}
             </LedgerTableBody>
           </LedgerTable>
-        </div>
-      </section>
+        </LedgerTableViewport>
+      </LedgerTableFrame>
 
       {syncOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate-900/40 p-4">
